4dba2f9a62880aecdd013db402e40659251d7571
YiyaCheng <cyiyacheng@163.com>
2019-03-14 23:16:55 +0800

    替换空格

---
+++ src/offerCode/Offer_replaceSpace.java
@@ -0,0 +1,29 @@
+package offerCode;
+
+import java.util.Scanner;
+
+public class Offer_replaceSpace {
+
+	/**
+	 * 	题目描述：请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+	  *          例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+	 */
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		System.out.println(replaceSpace(s));
+	}
+	
+	public static String replaceSpace(String str) {
+    	String res="";
+    	for(int i=0;i<str.length();i++){
+    		char c=str.charAt(i);
+    		if(c==' ')
+    			res+="%20";
+    		else 
+    			res+=c;
+    	}
+    	return res;
+    }
+
+}
